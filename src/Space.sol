9f9b64f4d6f19ed82c9714aa2c263faa3abf9631
Alexander Gonzalez <alexfertel97@gmail.com>
2022-04-13 16:31:36 +0200

    ref: remove ending periods

--- src/Space.sol
+++ src/Space.sol
@@ -84,10 +84,10 @@ contract Space is IMinimalSwapInfoPool, BalancerPoolToken, PoolPriceOracle {
     /// @notice Yieldspace config indicating the starting point from which the curve shifts (lower numbers mean that it starts closer to the constant sum side)
     uint256 public immutable ts;
 
-    /// @notice Yieldspace config indicating how much to discount the effective interest when swapping Target to PT.
+    /// @notice Yieldspace config indicating how much to discount the effective interest when swapping Target to PT
     uint256 public immutable g1;
 
-    /// @notice Yieldspace config indicating how much to mark the effective interest up when swapping PT to Target.
+    /// @notice Yieldspace config indicating how much to mark the effective interest up when swapping PT to Target
     uint256 public immutable g2;
 
     /* ========== INTERNAL IMMUTABLES ========== */
